590bf3a08ea75df0ba7cd10e8d16f7e47303c33f
Russell Hrycko <simpleeyeholdings@gmail.com>
2018-01-02 17:57:08 -0700

    adjusted app.html.erb links and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root 'root_home'
-  get 'About' => 'pages_about'
-  get 'Contact' => 'pages_contact'
+  get 'About', => 'pages#about'
+  get 'Contact', => 'pages#contact'
   
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

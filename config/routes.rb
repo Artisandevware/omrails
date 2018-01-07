35e19d3933c6630602aee44fdfcaa749d659f36f
Russell Hrycko <simpleeyeholdings@gmail.com>
2018-01-06 23:27:09 -0700

    Ditched About page

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
   root 'pages#home'
   get 'blog' => 'pages#blog'
-  get 'about' => 'pages#about'
   get 'contact' => 'pages#contact'
   
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html

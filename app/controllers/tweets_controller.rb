fcd5a83892a784f3decdcf817dd7d4d8d7a1dcca
Russell Hrycko <simpleeyeholdings@gmail.com>
2018-01-21 18:02:42 -0700

    changed the tweets controller

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,5 +1,5 @@
 class TweetsController < ApplicationController
-  before_action :authenticate_user!, except: [:index, :show]
+  before_action :set_tweet, only: [:show, :edit, :update, :destroy]
 
   # GET /tweets
   def index
@@ -8,7 +8,7 @@ class TweetsController < ApplicationController
 
   # GET /tweets/1
   def show
-    @tweet = Tweet.find(params[:id])
+  #  @tweet = Tweet.find(params[:id])
   end
 
   # GET /tweets/new
@@ -18,41 +18,52 @@ class TweetsController < ApplicationController
 
   # GET /tweets/1/edit
   def edit
-    @tweet = current_user.tweets.find(params[:id])
+    #@tweet = current_user.tweets.find(params[:id])
   end
 
   # POST /tweets
   def create
-    @tweet = current_user.tweets.new(tweet_params)
+    @tweet = Tweet.new(tweet_params)
+    respond_to do |format|
     if @tweet.save
-      redirect_to @tweet, notice: 'Tweet was successfully created.'
+      format.html { redirect_to @tweet, notice: 'Tweet was successfully created.'}
+      format.json { render :show, status: :created, location: @tweet }
     else
-      render :new 
+      format.html { render :new } 
+      format.json { render json: @tweet.errors, status: :unprocessable }
     end
   end
-
+end
   # PATCH/PUT /tweets/1
   def update
-    @tweet = current_user.tweets.find(params[:id])
+    respond_to do |format|
+    #@tweet = current_user.tweets.find(params[:id])
     if @tweet.update(tweet_params)
-      redirect_to @tweet, notice: 'Tweet was successfully updated.'
+      format.html { redirect_to @tweet, notice: 'Tweet was successfully updated.'}
+      format.json { render :show, status: :ok, location: @tweet } 
     else
-      render :edit
+      format.html { render :edit }
+      format.json { render json: @tweet.errors, status: :unprocessable }
+    end 
     end
   end
 
   # DELETE /tweets/1
   def destroy
-    @tweet = current_user.tweets.find(params[:id])
     @tweet.destroy
-    redirect_to tweets_url, notice: 'Tweet was successfully destroyed.'
+    respond_to do |format|
+    #@tweet = current_user.tweets.find(params[:id])
+    format.html { redirect_to tweets_url, notice: 'Tweet was successfully destroyed.' }
+    format.json { head :no_content }
   end
+end
 
   private
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def tweet_params
-      params.require(:tweet).permit(:content)
+      params.require(:tweet).permit(:user_id, :content)
     end
-end
+  end
+
 
